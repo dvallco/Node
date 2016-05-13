69f6eae505373b22211676d7d2d1c2539bc33f1f
David Vallcorba <David.vallcorba@scmspain.com>
2016-05-13 09:50:31 +0200

    calculator working

--- calculator.js
+++ calculator.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/suma', function (req, res) {
-  var num1 = req.arg1;
-  var num2 = req.arg2;
+  var num1 = Number(req.query.arg1);
+  var num2 = Number(req.query.arg2);
   var resultado = num1 + num2;
   res.send('Suma =' + resultado);
 });
